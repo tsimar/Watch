0ac5e54501c19ae00decdaf690082352d7697d0e
Volodymyr <coremio3@gmail.com>
2022-09-05 23:11:20 +0200

    i

---
+++ app.js
@@ -0,0 +1,26 @@
+
+class Clock {
+  constructor() {
+    this.hour = document.querySelector(".hour"); 
+    this.minute = document.querySelector(".minute"); 
+    this.second = document.querySelector(".second"); 
+  }
+
+  tick = () => {
+    let date = new Date();
+    let h = date.getHours() * 30;
+    let m = date.getMinutes() * 6;
+    let s = date.getSeconds() * 6; 
+
+    this.hour.style.transform = `rotateZ(${h + m/12}deg)`;
+    this.minute.style.transform = `rotateZ(${m}deg)`;
+    this.second.style.transform = `rotateZ(${s}deg)`;
+  }
+
+  start() {
+    setInterval(this.tick, 1000);
+  }
+}
+
+const clock = new Clock();
+clock.start();
